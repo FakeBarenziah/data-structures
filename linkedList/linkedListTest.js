835845866b52047bee2599a1d12d6d1bdc4b2099
Nick <nicholas.balestrieri@gmail.com>
2020-01-06 14:28:53 -0500

    Revise test file to demonstrate the other methods as well.

--- linkedList/linkedListTest.js
+++ linkedList/linkedListTest.js
@@ -14,9 +14,26 @@ myList.show()
 myList.reverse()
 myList.show()
 
+myList.deleteNode(2)
+myList.show()
+
+console.log(myList.get(1).value)
+myList.set(1, "B")
+console.log(myList.get(1).value)
+
+myList.show()
+console.log(myList.pop())
+myList.show()
 
 myListWithOneNode.insert(1)
 
 myListWithOneNode.show()
+
 myListWithOneNode.reverse()
+myListWithOneNode.show()
+
+myListWithOneNode.deleteNode(1)
+myListWithOneNode.show()
+
+myListWithOneNode.deleteNode(0)
 myListWithOneNode.show()
